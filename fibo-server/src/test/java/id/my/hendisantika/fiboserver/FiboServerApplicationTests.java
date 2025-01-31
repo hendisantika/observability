3d3547e22a49ca6ac971d3f324d6d60382bf9e8c
hendisantika <hendisantika@yahoo.co.id>
2025-02-01 06:12:46 +0700

    feat: Add fibo-server🫸🌀✏️📗🐧🐳⬆

---
+++ fibo-server/src/test/java/id/my/hendisantika/fiboserver/FiboServerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.fiboserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiboServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
