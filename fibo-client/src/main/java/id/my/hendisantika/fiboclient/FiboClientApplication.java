0512da2da5de2f53536fd34a4cecb9d9ff6423b5
hendisantika <hendisantika@yahoo.co.id>
2025-02-01 06:20:52 +0700

    feat: Add fibo-client🫸🌀✏️📗🐧🐳⬆

---
+++ fibo-client/src/main/java/id/my/hendisantika/fiboclient/FiboClientApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.fiboclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiboClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiboClientApplication.class, args);
+    }
+
+}
---
+++ fibo-client/src/test/java/id/my/hendisantika/fiboclient/FiboClientApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.fiboclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiboClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
