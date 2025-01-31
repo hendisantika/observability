97e22a61b1d7e05bf4aed5ddd2bf96d68f251094
hendisantika <hendisantika@yahoo.co.id>
2025-02-01 06:16:18 +0700

    feat: Add SampleInitializer🫸🌀✏️📗🐧🐳⬆

--- fibo-server/src/main/java/id/my/hendisantika/fiboserver/FiboServerApplication.java
+++ fibo-server/src/main/java/id/my/hendisantika/fiboserver/FiboServerApplication.java
@@ -1,7 +1,11 @@
 package id.my.hendisantika.fiboserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class FiboServerApplication {
@@ -10,4 +14,14 @@ public class FiboServerApplication {
 		SpringApplication.run(FiboServerApplication.class, args);
 	}
 
+}
+
+// INITIALIZE
+@Slf4j
+@Component
+class SampleInitializer {
+	@EventListener(ApplicationReadyEvent.class)
+	public void ready() {
+		log.info("Fibo Server Working ...");
+	}
 }
