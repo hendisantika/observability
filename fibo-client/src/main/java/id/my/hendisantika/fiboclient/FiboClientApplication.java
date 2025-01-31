465b199a6b530de114f1dc835ef531a996fad2e6
hendisantika <hendisantika@yahoo.co.id>
2025-02-01 06:23:30 +0700

    feat: Add WebClientConfig🫸🌀✏️📗🐧🐳⬆

--- fibo-client/src/main/java/id/my/hendisantika/fiboclient/FiboClientApplication.java
+++ fibo-client/src/main/java/id/my/hendisantika/fiboclient/FiboClientApplication.java
@@ -1,7 +1,18 @@
 package id.my.hendisantika.fiboclient;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.handler.logging.LogLevel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @SpringBootApplication
 public class FiboClientApplication {
@@ -10,4 +21,32 @@ public class FiboClientApplication {
         SpringApplication.run(FiboClientApplication.class, args);
     }
 
+}
+
+@Configuration
+class WebClientConfig {
+    @Bean
+    public WebClient webClientDefault() {
+        var factory = new DefaultUriBuilderFactory();
+        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+
+        // 16 MB
+        final int byteCount = 16 * 1024 * 1024;
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(byteCount))
+                .build();
+        ObjectMapper mapper = new ObjectMapper();
+        final HttpClient client = HttpClient.create()
+                .wiretap(this.getClass().getCanonicalName(), LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL);
+
+        return WebClient
+                .builder()
+                .exchangeStrategies(strategies)
+                .clientConnector(new ReactorClientHttpConnector(client))
+                .codecs(clientCodecConfigurer ->
+                        clientCodecConfigurer.defaultCodecs()
+                                .jackson2JsonDecoder(new Jackson2JsonDecoder(mapper)))
+                .uriBuilderFactory(factory)
+                .build();
+    }
 }
