683dde03505385bb3d533315def7d75cfc51c0e2
hendisantika <hendisantika@yahoo.co.id>
2025-02-01 06:16:54 +0700

    feat: Add postFibonacci in SampleService🫸🌀✏️📗🐧🐳⬆

--- fibo-server/src/main/java/id/my/hendisantika/fiboserver/FiboServerApplication.java
+++ fibo-server/src/main/java/id/my/hendisantika/fiboserver/FiboServerApplication.java
@@ -8,6 +8,10 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.List;
 
 @SpringBootApplication
 public class FiboServerApplication {
@@ -32,5 +36,13 @@ class SampleInitializer {
 @Service
 @AllArgsConstructor
 class SampleService {
-
+	public Flux<FiboResponse> postFibonacci(List<Integer> numbers) {
+		return Flux.fromIterable(numbers)
+				// One of my misunderstanding, async doesn't mean in parallel. To be parallel
+				// need declare it.
+				.parallel()
+				.runOn(Schedulers.boundedElastic())
+				.flatMap(number -> Flux.just(new FiboResponse(number, fibonacciOf(number))))
+				.sequential();
+	}
 }
