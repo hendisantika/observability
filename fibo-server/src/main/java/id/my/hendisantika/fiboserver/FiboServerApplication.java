4ecde0cfd156b1700b083ffc5c0563f1d9604730
hendisantika <hendisantika@yahoo.co.id>
2025-02-01 06:16:39 +0700

    feat: Add SampleService🫸🌀✏️📗🐧🐳⬆

--- fibo-server/src/main/java/id/my/hendisantika/fiboserver/FiboServerApplication.java
+++ fibo-server/src/main/java/id/my/hendisantika/fiboserver/FiboServerApplication.java
@@ -1,11 +1,13 @@
 package id.my.hendisantika.fiboserver;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class FiboServerApplication {
@@ -24,4 +26,11 @@ class SampleInitializer {
 	public void ready() {
 		log.info("Fibo Server Working ...");
 	}
+}
+
+// SERVICE
+@Service
+@AllArgsConstructor
+class SampleService {
+
 }
